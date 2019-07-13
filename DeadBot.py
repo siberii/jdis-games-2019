0ff4a06817e380f607aae0914c7fadd5a1f40b3a
HPay <hakimpayman@gmail.com>
2019-07-13 13:34:38 -0400

    HP- map middle point attribute added

--- DeadBot.py
+++ DeadBot.py
@@ -14,81 +14,8 @@
 import random
 from pacman.game import Directions
 import pacman.util as util # Free utility functions like Stack or Queue ! 
-from typing import Tuple, List
 from pacman.capture import GameState
 from pacman.captureAgents import CaptureAgent
-
-
-def isAlreadyBetter(cell,queue):
-    for element in queue:
-        if cell[0]==element[0]:
-            if element[1]<cell[1]:
-                return True
-            else:
-                return False 
-    return False
-    
-
-def isInList(myList, point)->bool:
-    for element in myList:
-        if element[0]==point:
-            return True
-    return False
-
-def getAdjacent(tile:Tuple[int,int])->List[Tuple[int,int]]:
-    x=tile[0]
-    y=tile[1]
-    listOfAdjacent=[]
-    listOfAdjacent.append((x+1,y))
-    listOfAdjacent.append((x,y+1))
-    listOfAdjacent.append((x-1,y))
-    listOfAdjacent.append((x,y-1))
-    return listOfAdjacent
-
-def findDirection(queue:List,origin: Tuple[int,int])->str:
-    reverseQueue= reversed(queue)
-    adjacentCells=getAdjacent(origin)
-    for element in reverseQueue:
-        if element[0] in adjacentCells:
-            if element[0][0]+1==origin[0]:
-                return Directions.WEST
-            elif element[0][0]-1==origin[0]:
-                return Directions.EAST
-            elif element[0][1]+1==origin[1]:
-                return Directions.SOUTH
-            else:
-                return Directions.NORTH
-    return
-
-
-
-
-
-def AddToQueue(tile:Tuple, queue, grid):
-    adjacentCells=getAdjacent(tile[0])
-    goodCells=[]
-    for cell in adjacentCells:
-        if(not grid[cell[0]][cell[1]]
-           and not isAlreadyBetter((cell,tile[1]+1),queue)):
-           goodCells.append((cell,tile[1]+1))
-    queue+=goodCells
-
-    
-def getDirectionAndDistance(fromPoint:Tuple[int,int],toPoint:Tuple[int,int], gamestate:GameState) -> Tuple[int,str]:
-    grid =gamestate.getWalls()
-
-    myList=[((toPoint[0],toPoint[1]),0)]
-
-    index=0
-    while (not isInList(myList,fromPoint)):
-        AddToQueue(myList[index], myList, grid)
-        index+=1
-    return (index,findDirection(myList,fromPoint))
-
-
-    
-
-    
 
 #################
 # Team creation #
@@ -109,10 +36,8 @@ def createTeam(firstIndex, secondIndex, isRed):
 # Agents #
 ##########
 
+#Attacking agent
 class AgentOne(CaptureAgent):
-    gridWall = []
-    mapMiddlePoint = []
-
     """
     A Dummy agent to serve as an example of the necessary agent structure.
     You should look at baselineTeam.py for more details about how to
@@ -143,35 +68,21 @@ class AgentOne(CaptureAgent):
         '''
         Your initialization code goes here, if you need any.
         '''
-        self.gridWall = gameState.getWalls()
-        self.mapMiddlePoint = (self.gridWall.width//2, self.gridWall.height//2)
-        print(self.mapMiddlePoint)
-        
 
     def chooseAction(self, gameState: GameState) -> str:
         """
         Picks among legal actions randomly.
         """
-        ownIndex=self.index
-        print(ownIndex)
-        ownPosition=gameState.getAgentPosition(ownIndex)
-        print(ownPosition)
-        destination = self.mapMiddlePoint
-        # if (ownIndex in gameState.getBlueTeamIndices()):
-        #     destination=gameState.getRedFood()[0]
-        # else:
-        #     destination=gameState.getBlueFood()[0]
+        
+        actions = gameState.getLegalActions(self.index)
+        return Directions.STOP
 
-        direction=getDirectionAndDistance(ownPosition,destination,gameState)[1]
-        print (direction)
-        return direction
-
-
-
+#defending agent
 class AgentTwo(CaptureAgent):
     def registerInitialState(self, gameState: GameState):
         CaptureAgent.registerInitialState(self, gameState)
     
     def chooseAction(self, gameState: GameState) -> str:
         actions = gameState.getLegalActions(self.index)
-        return random.choice(actions)
+        return Directions.STOP
+        
